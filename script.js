b1f4c71709a4240b7024cddde6f388b96306b7bf
Gustavo Henrique Ferreira Cruz <gustavohferreiracruz@gmail.com>
2022-07-05 00:13:27 -0300

    done

---
+++ script.js
@@ -0,0 +1,339 @@
+const peaoBrancoValores = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+  [0.1, 0.1, 0.2, 0.3, 0.3, 0.2, 0.1, 0.1],
+  [0.05, 0.05, 0.1, 0.25, 0.25, 0.1, 0.05, 0.05],
+  [0.0, 0.0, 0.0, 0.2, 0.2, 0.0, 0.0, 0.0],
+  [0.05, -0.05, -0.1, 0.0, 0.0, -0.1, -0.05, 0.05],
+  [0.05, 0.1, 0.1, -0.2, -0.2, 0.1, 0.1, 0.05],
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+];
+
+const peaoPretoValores = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5],
+  [0.1, 0.1, 0.2, 0.3, 0.3, 0.2, 0.1, 0.1],
+  [0.05, 0.05, 0.1, 0.25, 0.25, 0.1, 0.05, 0.05],
+  [0.0, 0.0, 0.0, 0.2, 0.2, 0.0, 0.0, 0.0],
+  [0.05, -0.05, -0.1, 0.0, 0.0, -0.1, -0.05, 0.05],
+  [0.05, 0.1, 0.1, -0.2, -0.2, 0.1, 0.1, 0.05],
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+];
+
+const cavaloBrancoValores = [
+  [-0.5, -0.4, -0.3, -0.3, -0.3, -0.3, -0.4, -0.5],
+  [-0.4, -0.2, 0.0, 0.0, 0.0, 0.0, -0.2, -0.4],
+  [-0.3, 0.0, 0.1, 0.15, 0.15, 0.1, 0.0, -0.3],
+  [-0.3, 0.05, 0.15, 0.2, 0.2, 0.15, 0.05, -0.3],
+  [-0.3, 0.0, 0.15, 0.2, 0.2, 0.15, 0.0, -0.3],
+  [-0.3, 0.05, 0.1, 0.15, 0.15, 0.1, 0.05, -0.3],
+  [-0.4, -0.2, 0.0, 0.05, 0.05, 0.0, -0.2, -0.4],
+  [-0.5, -0.4, -0.3, -0.3, -0.3, -0.3, -0.4, -0.5],
+];
+
+const cavaloPretoValores = [
+  [-0.5, -0.4, -0.3, -0.3, -0.3, -0.3, -0.4, -0.5],
+  [-0.4, -0.2, 0.0, 0.0, 0.0, 0.0, -0.2, -0.4],
+  [-0.3, 0.0, 0.1, 0.15, 0.15, 0.1, 0.0, -0.3],
+  [-0.3, 0.05, 0.15, 0.2, 0.2, 0.15, 0.05, -0.3],
+  [-0.3, 0.0, 0.15, 0.2, 0.2, 0.15, 0.0, -0.3],
+  [-0.3, 0.05, 0.1, 0.15, 0.15, 0.1, 0.05, -0.3],
+  [-0.4, -0.2, 0.0, 0.05, 0.05, 0.0, -0.2, -0.4],
+  [-0.5, -0.4, -0.3, -0.3, -0.3, -0.3, -0.4, -0.5],
+];
+
+const bispoBrancoValores = [
+  [-0.2, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.2],
+  [-0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.1],
+  [-0.1, 0.0, 0.05, 0.1, 0.1, 0.05, 0.0, -0.1],
+  [-0.1, 0.05, 0.05, 0.1, 0.1, 0.05, 0.05, -0.1],
+  [-0.1, 0.0, 0.1, 0.1, 0.1, 0.1, 0.0, -0.1],
+  [-0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, -0.1],
+  [-0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.05, -0.1],
+  [-0.2, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.2],
+];
+
+const bispoPretoValores = [
+  [-0.2, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.2],
+  [-0.1, 0.05, 0.0, 0.0, 0.0, 0.0, 0.05, -0.1],
+  [-0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, -0.1],
+  [-0.1, 0.0, 0.1, 0.1, 0.1, 0.1, 0.0, -0.1],
+  [-0.1, 0.05, 0.05, 0.1, 0.1, 0.05, 0.05, -0.1],
+  [-0.1, 0.0, 0.05, 0.1, 0.1, 0.05, 0.0, -0.1],
+  [-0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.1],
+  [-0.2, -0.1, -0.1, -0.1, -0.1, -0.1, -0.1, -0.2],
+];
+
+const torreBrancaValores = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [0.05, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [0.0, 0.0, 0.0, 0.05, 0.05, 0.0, 0.0, 0.0],
+];
+
+const torrePretaValores = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [0.05, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [-0.05, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.05],
+  [0.0, 0.0, 0.0, 0.05, 0.05, 0.0, 0.0, 0.0],
+];
+
+const rainhaBrancaValores = [
+  [-0.2, -0.1, -0.1, -0.05, -0.05, -0.1, -0.1, -0.2],
+  [-0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.1],
+  [-0.1, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.1],
+  [-0.05, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.05],
+  [0.0, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.05],
+  [-0.1, 0.05, 0.05, 0.05, 0.05, 0.05, 0.0, -0.1],
+  [-0.1, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0, -0.1],
+  [-0.2, -0.1, -0.1, -0.05, -0.05, -0.1, -0.1, -0.2],
+];
+
+const rainhaPretaValores = [
+  [-0.2, -0.1, -0.1, -0.05, -0.05, -0.1, -0.1, -0.2],
+  [-0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.1],
+  [-0.1, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.1],
+  [-0.05, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.05],
+  [0.0, 0.0, 0.05, 0.05, 0.05, 0.05, 0.0, -0.05],
+  [-0.1, 0.05, 0.05, 0.05, 0.05, 0.05, 0.0, -0.1],
+  [-0.1, 0.0, 0.05, 0.0, 0.0, 0.0, 0.0, -0.1],
+  [-0.2, -0.1, -0.1, -0.05, -0.05, -0.1, -0.1, -0.2],
+];
+
+const reiBrancoValores = [
+  [-0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3],
+  [-0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3],
+  [-0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3],
+  [-0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3],
+  [-0.2, -0.3, -0.3, -0.4, -0.4, -0.3, -0.3, -0.2],
+  [-0.1, -0.2, -0.2, -0.2, -0.2, -0.2, -0.2, -0.1],
+  [0.2, 0.2, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2],
+  [0.2, 0.3, 0.1, 0.0, 0.0, 0.1, 0.3, 0.2],
+];
+
+const reiPretoValores = [
+  [-0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3],
+  [-0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3],
+  [-0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3],
+  [-0.3, -0.4, -0.4, -0.5, -0.5, -0.4, -0.4, -0.3],
+  [-0.2, -0.3, -0.3, -0.4, -0.4, -0.3, -0.3, -0.2],
+  [-0.1, -0.2, -0.2, -0.2, -0.2, -0.2, -0.2, -0.1],
+  [0.2, 0.2, 0.0, 0.0, 0.0, 0.0, 0.2, 0.2],
+  [0.2, 0.3, 0.1, 0.0, 0.0, 0.1, 0.3, 0.2],
+];
+
+let tabuleiro,
+  xadrez = new Chess();
+
+let algoritmo = "minimax";
+
+let corJogador = "white";
+
+let brancas = true,
+  pretas = false;
+
+const PROFUNDIDADE = 3;
+
+const algoritmoAleatorio = function (xadrez) {
+  const novasJogadas = xadrez.moves();
+  return novasJogadas[Math.floor(Math.random() * novasJogadas.length)];
+};
+
+const minimaxRaiz = function (profundidade, xadrez, jogadorMax) {
+  const novaJogadas = xadrez.moves();
+  let melhorMovimento = jogadorMax ? -Infinity : Infinity;
+  let melhorMovimentoEncontrado;
+  novaJogadas.forEach((novaJogada) => {
+    const jogada = novaJogada;
+    xadrez.move(jogada);
+    const valor = minimax(profundidade - 1, xadrez, -10000, 10000, !jogadorMax);
+    xadrez.undo();
+    if (jogadorMax && valor >= melhorMovimento) {
+      melhorMovimento = valor;
+      melhorMovimentoEncontrado = jogada;
+    } else if (!jogadorMax && valor <= melhorMovimento) {
+      melhorMovimento = valor;
+      melhorMovimentoEncontrado = jogada;
+    }
+  });
+  return melhorMovimentoEncontrado;
+};
+
+const minimax = function (profundidade, xadrez, alfa, beta, jogadorMax) {
+  if (profundidade === 0) {
+    return -avaliarTabuleiro(xadrez.board());
+  }
+  const novaJogadas = xadrez.moves();
+  if (jogadorMax) {
+    let melhorMovimento = -Infinity;
+    novaJogadas.forEach((novaJogada) => {
+      xadrez.move(novaJogada);
+      melhorMovimento = Math.max(
+        melhorMovimento,
+        minimax(profundidade - 1, xadrez, alfa, beta, !jogadorMax)
+      );
+      xadrez.undo();
+      alfa = Math.max(alfa, melhorMovimento);
+      if (beta <= alfa) {
+        return melhorMovimento;
+      }
+    });
+    return melhorMovimento;
+  } else {
+    let melhorMovimento = Infinity;
+    for (let i = 0; i < novaJogadas.length; i++) {
+      xadrez.move(novaJogadas[i]);
+      melhorMovimento = Math.min(
+        melhorMovimento,
+        minimax(profundidade - 1, xadrez, alfa, beta, !jogadorMax)
+      );
+      xadrez.undo();
+      beta = Math.min(beta, melhorMovimento);
+      if (beta <= alfa) {
+        return melhorMovimento;
+      }
+    }
+    return melhorMovimento;
+  }
+};
+
+const avaliarTabuleiro = function (tabuleiro) {
+  let valorMaximo = 0;
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      valorMaximo = valorMaximo + calcularValorPeca(tabuleiro[i][j], i, j);
+    }
+  }
+  return valorMaximo;
+};
+
+const calcularValorPeca = function (peca, x, y) {
+  if (peca === null) {
+    return 0;
+  }
+  const getValorAbsoluto = function (peca, pecaBranca, x, y) {
+    if (peca.type === "p") {
+      return (
+        1 + (pecaBranca ? peaoBrancoValores[y][x] : peaoPretoValores[y][x])
+      );
+    } else if (peca.type === "n") {
+      return (
+        3 + (pecaBranca ? cavaloBrancoValores[y][x] : cavaloPretoValores[y][x])
+      );
+    } else if (peca.type === "b") {
+      return (
+        3 + (pecaBranca ? bispoBrancoValores[y][x] : bispoPretoValores[y][x])
+      );
+    } else if (peca.type === "r") {
+      return (
+        5 + (pecaBranca ? torreBrancaValores[y][x] : torrePretaValores[y][x])
+      );
+    } else if (peca.type === "q") {
+      return (
+        9 + (pecaBranca ? rainhaBrancaValores[y][x] : rainhaPretaValores[y][x])
+      );
+    } else if (peca.type === "k") {
+      return 90 + (pecaBranca ? reiBrancoValores[y][x] : reiPretoValores[y][x]);
+    }
+  };
+
+  const valorAbsoluto = getValorAbsoluto(peca, peca.color === "w", x, y);
+  return peca.color === "w" ? valorAbsoluto : -valorAbsoluto;
+};
+
+const onDragStart = function (_, peca, _, orientacao) {
+  const corPeca =
+    orientacao === "white"
+      ? peca.search(/^b/) !== -1
+      : peca.search(/^w/) !== -1;
+  if (xadrez.in_checkmate() === true || xadrez.in_draw() === true || corPeca) {
+    return false;
+  }
+};
+
+const fazMelhorJogada = function () {
+  const melhorMovimento = getMelhorJogada(xadrez);
+  xadrez.move(melhorMovimento);
+  tabuleiro.position(xadrez.fen());
+  if (xadrez.game_over()) {
+    alert("Fim de jogo!");
+  }
+};
+
+const getMelhorJogada = function (xadrez) {
+  if (xadrez.game_over()) {
+    alert("Fim de jogo!");
+  }
+  const jogadorMax = corJogador === "white";
+  const melhorMovimento =
+    algoritmo === "minimax"
+      ? minimaxRaiz(PROFUNDIDADE, xadrez, jogadorMax)
+      : algoritmoAleatorio(xadrez);
+  return melhorMovimento;
+};
+
+const onDrop = function (source, target) {
+  const movimento = xadrez.move({
+    from: source,
+    to: target,
+    promotion: "q",
+  });
+
+  if (movimento === null) {
+    return "snapback";
+  }
+  window.setTimeout(fazMelhorJogada, 250);
+};
+
+const onSnapEnd = function () {
+  tabuleiro.position(xadrez.fen());
+};
+
+const cfg = {
+  draggable: true,
+  position: "start",
+  onDragStart,
+  onDrop,
+  onSnapEnd,
+};
+tabuleiro = ChessBoard("tabuleiro", cfg);
+
+$("#brancas").on("click", () => {
+  cfg.orientation = "white";
+  corJogador = "white";
+  $("#brancas").addClass("botaoSelecionado");
+  $("#pretas").removeClass("botaoSelecionado");
+});
+
+$("#pretas").on("click", () => {
+  cfg.orientation = "black";
+  corJogador = "black";
+  $("#pretas").addClass("botaoSelecionado");
+  $("#brancas").removeClass("botaoSelecionado");
+});
+
+$("#minimax").on("click", () => {
+  algoritmo = "minimax";
+  $("#minimax").addClass("botaoSelecionado");
+  $("#aleatorio").removeClass("botaoSelecionado");
+});
+
+$("#aleatorio").on("click", () => {
+  algoritmo = "aleatorio";
+  $("#aleatorio").addClass("botaoSelecionado");
+  $("#minimax").removeClass("botaoSelecionado");
+});
+
+$("#iniciar").on("click", () => {
+  tabuleiro = ChessBoard("tabuleiro", cfg);
+  xadrez = Chess();
+  if (corJogador === "black") window.setTimeout(fazMelhorJogada, 250);
+});
